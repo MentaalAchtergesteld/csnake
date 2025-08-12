657a53567636ea4dd14ef220b2afa423a07f502e
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-08-12 21:31:32 +0200

    added dying, and resetting

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "raylib.h"
@@ -17,6 +18,8 @@ typedef struct {
 } Snake;
 
 typedef struct {
+	bool lost;
+
 	int gridSize;	
 	int cellSize;
 
@@ -52,8 +55,7 @@ void moveSnake(Snake *snake) {
         CellNode *next = cell->next;
         if (next == NULL) break; 
 
-        cell->data.x = next->data.x;
-        cell->data.y = next->data.y;
+        cell->data = next->data;
 
         cell = next;
     }
@@ -64,25 +66,31 @@ void moveSnake(Snake *snake) {
 
 void growSnake(Snake *snake, Vector2 tail) {
 	CellNode *newTail = malloc(sizeof(CellNode));
-	if (!newTail) {
-		printf("ERROR: couldn't allocate CellNode in growing snake");
-		exit(1);
-	}
-
-	Vector2 *pos = malloc(sizeof(Vector2));
-	if (!pos) {
-		free(newTail);
-		printf("ERROR: couldn't allocate Vector2 in growing snake");
-		exit(1);
-	}
-
-	*pos = tail;
-
-	newTail->data = *pos;
+	newTail->data = tail;
 	newTail->next = snake->tail;
 
 	snake->tail = newTail;
 	snake->length += 1;
+}
+
+void resetSnake(Snake *snake) {
+	CellNode* cell = snake->tail;
+
+	while (cell != NULL) {
+		CellNode* next = cell->next;
+		free(cell);
+		cell = next;
+	}
+
+	snake->tail = NULL;
+	growSnake(snake, (Vector2){ 2, 0 });
+	snake->head = snake->tail;
+	growSnake(snake, (Vector2){ 1, 0 });
+	growSnake(snake, (Vector2){ 0, 0 });
+
+	snake->length = 3;
+	snake->direction = (Vector2){ 1, 0 };
+	snake->desiredDirection = (Vector2){ 1, 0 };
 }
 
 void randomizeFood(Vector2 *food, int gridSize) {
@@ -99,6 +107,33 @@ void updateGame(GameState *state) {
 		tail = state->snake.tail->data;
 		state->snake.direction = state->snake.desiredDirection;
 		moveSnake(&state->snake);
+
+		CellNode* head = state->snake.head;
+		CellNode* cell = state->snake.tail;
+		while (cell != NULL) {
+			CellNode* next = cell->next;
+			if ( next == NULL) break;
+
+			if (cell->data.x == head->data.x && cell->data.y == head->data.y) {
+				state->lost = true;
+				break;
+			}
+
+			cell = next;
+		}
+
+		if (state->snake.head->data.x < 0 || state->snake.head->data.x >= state->gridSize) {
+			state->lost = true;
+			return;
+		}
+
+		if (state->snake.head->data.y < 0 || state->snake.head->data.y >= state->gridSize) {
+			printf("Y\n");
+			state->lost = true;
+			return;
+		}
+
+
 		if (state->snake.head->data.x == state->food.x && state->snake.head->data.y == state->food.y) {
 			growSnake(&state->snake, tail);
 			randomizeFood(&state->food, state->gridSize);
@@ -115,6 +150,7 @@ void updateGame(GameState *state) {
 } 
 
 int main() {
+	srand(time(NULL));
 	const int windowSize = 1024;
 	const int targetFPS = 60;
 
@@ -123,35 +159,51 @@ int main() {
 
 	const int gridOffset = (windowSize - gridSize * cellSize)/2;
 
-	Snake snake;
-	snake.length = 2;
-	snake.direction = (Vector2){ 1, 0 };
-	snake.desiredDirection = (Vector2){ 1, 0 };
+	GameState state = {
+		.lost = false,
+		.snake = {
+			.length = 3,
+			.direction = { 1, 0 },
+			.desiredDirection = state.snake.direction, 
+		},
+		.gridSize = gridSize,
+		.cellSize = cellSize,
+		.baseSpeed = 0.25,
+		.speedFactor = 0.95,
+		.currentSpeed = state.baseSpeed,
+		.food = { 0, 0 },
+		.inc = 0
+	};
 
-	growSnake(&snake, (Vector2){ 1, 0 });
-	snake.head = snake.tail;
-	growSnake(&snake, (Vector2){ 0, 0 });
-
-	Vector2 food;
-	randomizeFood(&food, gridSize);
-
-	GameState state;
-	state.gridSize = gridSize;
-	state.cellSize = cellSize;
-
-	state.baseSpeed = 0.25;
-	state.speedFactor = 0.95;
-	state.currentSpeed = state.baseSpeed;
-	state.snake = snake;
-	state.food = food;
-	state.inc = 0;
+	resetSnake(&state.snake);
+	randomizeFood(&state.food, gridSize);
 
 	InitWindow(windowSize, windowSize, "C Snake");
 	SetTargetFPS(targetFPS);
 
 	while (!WindowShouldClose()) {
-
 		updateGame(&state);
+
+		while (state.lost) {
+			BeginDrawing();
+			char *message = "You lost, press space to play again";
+			int fontSize = 32;
+			unsigned int length = MeasureText(message, fontSize);
+			DrawText(
+					message,
+					windowSize/2 - length / 2, windowSize/2  - fontSize/2,
+					fontSize, BLACK
+			);
+			EndDrawing();
+			if (IsKeyPressed(KEY_SPACE)) {
+				state.lost = false;
+				state.inc = 0;
+				state.currentSpeed = state.baseSpeed;
+				resetSnake(&state.snake);
+				randomizeFood(&state.food, state.gridSize);
+			}
+			if (WindowShouldClose()) break;
+		}
 
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
@@ -190,7 +242,7 @@ int main() {
 			cell = cell->next;
 		}
 
-		Vector2 pos = gridToScreen(snake.head->data, cellSize);
+		Vector2 pos = gridToScreen(state.snake.head->data, cellSize);
 		DrawRectangle(gridOffset+pos.x, gridOffset+pos.y, cellSize, cellSize, BLUE);
 		EndDrawing();
 	}
